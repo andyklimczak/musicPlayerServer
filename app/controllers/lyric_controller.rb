3583e0a2bafa94dcff7342496433963a0874cfbb
andy <andyklimczak@gmail.com>
2014-10-08 10:01:00 -0400

    initial commit

---
+++ app/controllers/lyric_controller.rb
@@ -0,0 +1,10 @@
+class LyricController < ApplicationController
+  #lyricswikia.com
+  # given song name
+  # returns lyrics
+  def get_lyrics
+    song = ActiveSupport::JSON.decode(params[:song_name].string)
+    render :json params
+  end
+end
+
