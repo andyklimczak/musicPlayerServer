98a274d4ad8160e44291824a5b48432ee99e5de8
andy <andyklimczak@gmail.com>
2014-10-10 00:59:51 -0400

    user login/signup, not sure if this is how it'll work though

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,2 +1,21 @@
 class UserController < ApplicationController
+  # post /create_user
+  # email, name, password
+  # returns user if password is valid, else nil
+  def create_user
+    user = User.create(email: params[:email], name: params[:name], password: params[:password])
+    render json: user
+  end
+
+  # post /login
+  # email, password
+  # returns user if password is valid, else nil
+  def login_as
+    user = User.where(email: params[:email])
+    if user.password == params[:password]
+      render json :user
+    else
+      render json: nil
+    end
+  end
 end
