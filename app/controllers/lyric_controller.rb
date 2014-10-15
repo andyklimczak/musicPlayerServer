be83cc04cff98c37f9cadb61d2f674f9923bdac9
andy <andyklimczak@gmail.com>
2014-10-14 23:20:50 -0400

    remove lyrics temporarily

--- app/controllers/lyric_controller.rb
+++ app/controllers/lyric_controller.rb
@@ -2,9 +2,9 @@ class LyricController < ApplicationController
   #lyricswikia.com
   # given song name
   # returns lyrics
-  def get_lyrics
-    song = ActiveSupport::JSON.decode(params[:song_name].string)
-    render :json params
-  end
+ # def get_lyrics
+  #  song = ActiveSupport::JSON.decode(params[:song_name].string)
+ #   render :json params
+#  end
 end
 
