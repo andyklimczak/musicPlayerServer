8be181252a386408741db974e9831ce8aa4430d8
andy <andyklimczak@gmail.com>
2014-10-30 23:20:07 -0400

    fix json response

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -4,10 +4,8 @@ class UserController < ApplicationController
   #returns the user
   def create
     user = User.create(facebook_id: params[:facebook_id], name: params[:name], email: params[:email])
-    logger = Logger.new('log/andy.log')
-    logger.debug(user)
-    if user.save
-      render json: user
+    if user.save!
+      render json: user.to_json
     else
       render json: nil
     end
@@ -16,11 +14,9 @@ class UserController < ApplicationController
   # params facebook_id
   # returns the user
   def get_user
-    user = User.find_by(facebook_id: params[:facebook_id]
-    logger = Logger.new('log/andy.log')
-    logger.debug(user))
+    user = User.find_by(facebook_id: params[:facebook_id])
     if not user.nil?
-      render json: user
+      render json: user.to_json
     else
       render json: nil
     end
