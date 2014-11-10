7720588a4d35ebe2b36d4acb6b1030e21a6be3d5
andy <andyklimczak@gmail.com>
2014-11-10 09:50:10 -0500

    go through hte passed in artist and song params

--- app/controllers/server_controller.rb
+++ app/controllers/server_controller.rb
@@ -1,12 +1,14 @@
 class ServerController < ApplicationController
   #post update_session
   #params: id, song : {title : count}, artist : { name : count }
-  def update_session #app.post('update_session', facebook_id: "123", songs: {title:"let it be", title:"stuff"})
+  #app.post('update_session', facebook_id: "123", songs: {songname1:"count1", songname2:"count2"}, artists: {artist1:"artist1count", artist2:"artist2count"})
+  def update_session
     user = User.where(facebook_id: params[:facebook_id]).first
     params[:songs].each do |song|
       p song
     end
-    # p params[:songs
-    # p params[:artists]
+    params[:artists].each do |artist|
+      p artist
+    end
   end
 end
