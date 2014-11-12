41f75a74ef837b3d39647493e48e83b156d9d119
andy <andyklimczak@gmail.com>
2014-11-11 20:43:18 -0500

    json

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -8,6 +8,8 @@ class ProfileController < ApplicationController
     artists = JSON.parse params[:artists]
     p songs
     p artists
+    p JSON.parse songs
+    p JSON.parse artists
     #user = User.where(facebook_id: params[:facebook_id]).first
     songs.each do |song_pair|
       p song
