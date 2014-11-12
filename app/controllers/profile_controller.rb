e5c6d47ae4a12111b486f3561b5dd890692bf0be
andy <andyklimczak@gmail.com>
2014-11-11 20:34:13 -0500

    params songs

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -4,7 +4,8 @@ class ProfileController < ApplicationController
   #app.post('update_session', facebook_id: "123", songs: {songname1:"count1", songname2:"count2"}, artists: {artist1:"artist1count", artist2:"artist2count"})
   def update_session
     p "i am here"
-    p JSON.parse params
+    p params[:songs]
+    p JSON.parse params[:songs]
     #user = User.where(facebook_id: params[:facebook_id]).first
     # params[:songs].each do |song|
     #   p song
