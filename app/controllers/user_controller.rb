149f669d4e055768cea64570384ba3295267f12b
andy <andyklimczak@gmail.com>
2014-11-11 21:47:59 -0500

    log user response

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -7,9 +7,6 @@ class UserController < ApplicationController
     if user.nil?
       user = User.create(facebook_id: params[:facebook_id], name: params[:name], email: params[:email])
     end
-    render json: => {
-      songs: user.songs_hash.as_json,
-      artists: user.artists_hash.as_json
-    }
+    render json: user.songs_hash.as_json,
   end
 end
