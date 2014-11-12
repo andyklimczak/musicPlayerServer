259b7fac01edf7a0ba50ab61df7ca853db1cd00d
andy <andyklimczak@gmail.com>
2014-11-11 20:24:18 -0500

    here we go

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -4,13 +4,12 @@ class ProfileController < ApplicationController
   #app.post('update_session', facebook_id: "123", songs: {songname1:"count1", songname2:"count2"}, artists: {artist1:"artist1count", artist2:"artist2count"})
   def update_session
     p "i am here"
-    p params
     #user = User.where(facebook_id: params[:facebook_id]).first
-    # params[:songs].each do |song|
-    #   p song
-    # end
-    # params[:artists].each do |artist|
-    #   p artist
-    #end
+    params[:songs].each do |song|
+      p song
+    end
+    params[:artists].each do |artist|
+      p artist
+    end
   end
 end
