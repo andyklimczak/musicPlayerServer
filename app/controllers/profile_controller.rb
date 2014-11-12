3ba2a5b562c8e4e6d2dfea5f1283066d295d1c42
andy <andyklimczak@gmail.com>
2014-11-11 20:58:35 -0500

    json type

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -4,16 +4,16 @@ class ProfileController < ApplicationController
   #app.post('update_session', facebook_id: "123", songs: {songname1:"count1", songname2:"count2"}, artists: {artist1:"artist1count", artist2:"artist2count"})
   def update_session
     p "i am here"
-    songs = JSON.parse params[:songs]
-    artists = JSON.parse params[:artists]
-    p songs
-    p artists
-    #user = User.where(facebook_id: params[:facebook_id]).first
-    songs.each do |song_pair|
-      p song_pair
-    end
-    artists.each do |artist_pair|
-      p artist_pair
-    end
+    # songs = JSON.parse params[:songs]
+    # artists = JSON.parse params[:artists]
+    # user = User.where(facebook_id: params[:facebook_id]).first
+    p params.class.name
+    # songs.each do |song_pair|
+    #   p song_pair
+
+    # end
+    # artists.each do |artist_pair|
+    #   p artist_pair
+    # end
   end
 end
