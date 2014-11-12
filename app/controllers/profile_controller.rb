1afccfea209c4f198ff1e3bd9756c72ac0180c87
andy <andyklimczak@gmail.com>
2014-11-12 16:34:20 -0500

    not perfect

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -3,9 +3,12 @@ class ProfileController < ApplicationController
   #params: id, song : {title : count}, artist : { name : count }
   #app.post('update_session', facebook_id: "123", songs: {songname1:"count1", songname2:"count2"}, artists: {artist1:"artist1count", artist2:"artist2count"})
   def update_session
+    p update_session
     user = User.where(facebook_id: params[:facebook_id]).first
     songs_hash = JSON.parse params[:songs]
     artists_hash = JSON.parse params[:artists]
+    p songs_hash
+    p artists_hash
     user.update(songs_hash: songs_hash.to_s)
     user.update(artists_hash: artists_hash.to_s)
     p songs_hash.to_s
